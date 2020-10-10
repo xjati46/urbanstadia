7e080094d3ca40acf17f65a2ff972e68ee923723
Ishaq Ahmad Nizhami <Nizhami@Nizhamis-MacBook-Air.local>
2020-10-10 23:38:23 +0700

    1st revision

--- urbanstadia/settings.py
+++ urbanstadia/settings.py
@@ -131,6 +131,7 @@ USE_THOUSAND_SEPARATOR = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL = '/main/'
 
